e0fa7269222239f149f5a842f95f345722b5f5a3
乔晓建 <qiaoxj@inspur.com>
2020-01-20 17:49:33 +0800

    'x'

--- advance_go_programming_book/p1/mian/demo.go
+++ advance_go_programming_book/p1/mian/demo.go
@@ -1,32 +1,31 @@
 package main
 
-import "fmt"
-
-func GenerateNatural() chan int {
-	ch := make(chan int)
-	go func() {
-		for i := 2; ; i++ {
-			ch <- i
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+func woker(ctx context.Context,wg *sync.WaitGroup) error{
+	defer wg.Done()
+	for{
+		select {
+		default:
+			fmt.Println("hello")
+		case <-ctx.Done():
+			return ctx.Err()
 		}
-	}()
-	return ch
-}
-func PrimeFilter(in <-chan int, prime int) chan int {
-	out := make(chan int)
-	go func() {
-		for {
-			if i := <-in; i%prime != 0 {
-				out <- i
-			}
-		}
-	}()
-	return out
+	}
 }
 func main() {
-	ch := GenerateNatural() // 自然数序列: 2, 3, 4, ...
-	for i := 0; i < 100; i++ {
-		prime := <-ch // 新出现的素数
-		fmt.Printf("%v: %v\n", i+1, prime)
-		ch = PrimeFilter(ch, prime) // 基于新素数构造的过滤器
+	ctx, cancel := context.WithTimeout(context.Background(), 10*
+		time.Second)
+	var wg sync.WaitGroup
+	for i := 0; i<10; i++{
+		wg.Add(1)
+		go woker(ctx,&wg)
 	}
+	time.Sleep(time.Second)
+	cancel()
+	wg.Wait()
 }
